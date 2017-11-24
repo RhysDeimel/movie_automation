1b0077ab118c1b67dcad284f526a43765eb843e6
RhysDeimel <deimelr@hotmail.com>
2017-11-24 15:13:48 +1100

    added print statements for progress

--- movie_automation.py
+++ movie_automation.py
@@ -152,6 +152,7 @@ def download_magnet(movie_details, magnet_link, dir='./'):
         return 'download completed' in output.decode()
 
     except subprocess.TimeoutExpired:
+        print('Trying to convert magnet link for {} took too long'.format(movie_details[0]))
         return False
     else:
         print('Something went wrong trying to download a magnet link')
@@ -180,12 +181,16 @@ def main():
         if not movie_in_DB(movie):
 
             try:
+                print('Downloading {}'.format(movie[0]))
                 download_torrent(movie)
                 write_DB(movie)
+                print('{} successfully downloaded and entered into DB'.format(movie[0]))
             except requests.exceptions.InvalidSchema as e:
+                print('{} is actually a magnet link, using aria')
                 magnet_link = catch_magnet(e.args)
                 if download_magnet(movie, magnet_link):
                     write_DB(movie)
+                    print('{} successfully downloaded and entered into DB'.format(movie[0]))
 
 
 
